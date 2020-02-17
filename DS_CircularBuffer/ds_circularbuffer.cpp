94b83b067c395a67ccd8942c26b97817fbb5c4d5
shuloh <lohshupeng@gmail.com>
2020-02-17 15:32:59 +0800

    implementation working build

--- DS_CircularBuffer/ds_circularbuffer.cpp
+++ DS_CircularBuffer/ds_circularbuffer.cpp
@@ -1,7 +1,74 @@
 #include "ds_circularbuffer.h"
-#include <iostream>
+#include <memory>
+#include <mutex>
 template <typename T>
-void circular_buffer<T>::test() {
-    using namespace std;
-    cout<<"testing";
+void circular_buffer<T>::clear()
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    head_ = tail_;
+    full_ = false;
+}
+
+template <typename T>
+bool circular_buffer<T>::empty() const
+{
+    return (!full_ && (head_ == tail_));
+}
+
+template <typename T>
+bool circular_buffer<T>::full() const
+{
+    return full_;
+}
+
+template <typename T>
+size_t circular_buffer<T>::capacity() const
+{
+    return full_;
+}
+
+template <typename T>
+size_t circular_buffer<T>::size() const
+{
+    if (full)
+    {
+        return max_size_;
+    }
+    else if (head_ >= tail_)
+    {
+        return head_ - tail_;
+    }
+    //writes have wrapped to the front of the array
+    //size is the tail index to the end of array + write index
+    else
+    {
+        return max_size_ - tail_ + head_;
+    }
+}
+
+template <typename T>
+void circular_buffer<T>::put(T item)
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (full_) {
+        tail_ = (tail_ + 1) % max_size_;
+    }
+    buf_[head_] = item;
+    head_ = (head_ + 1) % max_size_;
+    full_ = head_ == tail_;
+}
+
+template <typename T>
+T circular_buffer<T>::get()
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (empty()) 
+    {
+        return T();
+    }
+    T result = buf_[tail_];
+    tail_ = (tail_ + 1) % max_size_;
+    //if tail_ == head_ then the buffer is empty (after read) -- will never be full
+    full_ = false;
+    return result;
 }
--- DS_CircularBuffer/ds_circularbuffer.h
+++ DS_CircularBuffer/ds_circularbuffer.h
@@ -1,11 +1,27 @@
 #include <memory>
+#include <mutex>
 template <typename T>
 class circular_buffer
 {
 public:
-    explicit circular_buffer(size_t size) : buf_(std::unique_ptr<T[]>(new T[size])){};
-    void test();
+    explicit circular_buffer(size_t size) : buf_{std::make_unique<T[]>(size)},
+                                            max_size_{size} {
+
+                                            };
+    void put(T item);
+    void put(T&& item);
+    T get() ;
+    void clear();
+    bool empty() const;
+    bool full() const;
+    size_t capacity() const;
+    size_t size() const;
 
 private:
+    std::mutex mutex_;
     std::unique_ptr<T[]> buf_;
+    size_t head_ = 0;
+    size_t tail_ = 0;
+    const size_t max_size_;
+    bool full_{false};
 };
